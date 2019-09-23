a300a56f9ccc8db8a4607437c973b16e4cb12d12
wangdongsheng <wangdongsheng@100tal.com>
2019-09-23 08:18:35 +0800

    提交

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //app.js
 const http = require('./utils/http.js');
-import {setH5url} from './utils/util';
+import {
+  setH5url
+} from './utils/util';
 App({
-  onLaunch: function(options) {
+  onLaunch: function (options) {
     console.log('app.js页面加载的');
     // this.authorize();
   },
@@ -10,6 +12,7 @@ App({
   globalData: {
     h5url: '', // web-view使用的h5地址
     webHost: 'http://localhost:2000',
+    // webHost: 'http://192.168.43.131:2000',
     // webHost: 'https://www.xiongjiajia.com/pages',
     backH5hash: '', // 跳回h5的地址
     userInfo: null,
